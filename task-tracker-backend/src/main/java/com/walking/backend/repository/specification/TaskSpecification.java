006cd6a0c4d9fa6045c024ee91a4668cfdbc039f
Zoxid-Iskandarov <iskandarovzoxid27@gmail.com>
2025-12-19 16:56:46 +0500

    Add type-safe JPA Criteria queries with Metamodel and move all dependency versions to gradle.properties

--- task-tracker-backend/src/main/java/com/walking/backend/repository/specification/TaskSpecification.java
+++ task-tracker-backend/src/main/java/com/walking/backend/repository/specification/TaskSpecification.java
@@ -1,6 +1,8 @@
 package com.walking.backend.repository.specification;
 
 import com.walking.backend.domain.model.Task;
+import com.walking.backend.domain.model.Task_;
+import com.walking.backend.domain.model.User_;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.time.LocalDateTime;
@@ -8,15 +10,17 @@ import java.time.LocalDateTime;
 public class TaskSpecification {
 
     public static Specification<Task> hasUserId(Long userId) {
-        return (root, query, cb) -> cb.equal(root.get("user").get("id"), userId);
+        return (root, query, cb)
+                -> cb.equal(root.get(Task_.user).get(User_.id), userId);
     }
 
     public static Specification<Task> isCompleted(Boolean completed) {
-        return (root, query, cb) -> cb.equal(root.get("isCompleted"), completed);
+        return (root, query, cb)
+                -> cb.equal(root.get(Task_.isCompleted), completed);
     }
 
     public static Specification<Task> hasTodayFlag() {
         return (root, query, cb)
-                -> cb.greaterThanOrEqualTo(root.get("created"), LocalDateTime.now().toLocalDate().atStartOfDay());
+                -> cb.greaterThanOrEqualTo(root.get(Task_.created), LocalDateTime.now().toLocalDate().atStartOfDay());
     }
 }
