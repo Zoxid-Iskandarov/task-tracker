5ba02972b86c62694152d3a6cc5453ffb70d430d
Zoxid-Iskandarov <iskandarovzoxid27@gmail.com>
2026-04-27 19:08:42 +0500

    feat(db): introduce board_member table and remove user_id from board

--- task-tracker-backend/src/main/resources/db/changelog/V1_0_2__create_board.sql
+++ task-tracker-backend/src/main/resources/db/changelog/V1_0_2__create_board.sql
@@ -2,8 +2,6 @@ CREATE TABLE board
 (
     id      BIGSERIAL PRIMARY KEY,
     name    VARCHAR(100) NOT NULL,
-    user_id BIGINT       NOT NULL REFERENCES users (id) ON DELETE CASCADE,
     created TIMESTAMP DEFAULT now(),
-    updated TIMESTAMP,
-    CONSTRAINT uk_board_name_user_id UNIQUE (name, user_id)
+    updated TIMESTAMP
 );
