8185e4aa85402be4f9f985f27f7d9a5d0da339ce
Zoxid-Iskandarov <iskandarovzoxid27@gmail.com>
2026-05-07 01:35:00 +0500

    refactor(kafka): move consumer/producer config from Java to YAML

--- task-tracker-email-sender/src/main/java/com/walking/emailSender/config/KafkaConsumerConfig.java
+++ task-tracker-email-sender/src/main/java/com/walking/emailSender/config/KafkaConsumerConfig.java
@@ -1,70 +1,31 @@
 package com.walking.emailSender.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.*;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
-import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
-import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
-import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.util.backoff.FixedBackOff;
 
 @Configuration
 public class KafkaConsumerConfig {
 
     @Bean
-    public ConsumerFactory<String, Object> consumerFactory(
-            @Value("${spring.kafka.consumer.bootstrap-servers}") String bootstrapServers,
-            @Value("${spring.kafka.consumer.group-id}") String groupId,
-            @Value("${spring.kafka.consumer.properties.spring.json.trusted.packages}") String trustedPackages,
-            @Value("${spring.kafka.consumer.properties.spring.json.type.mapping}") String typeMappings
-    ) {
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        configs.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
-        configs.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JacksonJsonDeserializer.class);
-        configs.put(JacksonJsonDeserializer.TRUSTED_PACKAGES, trustedPackages);
-        configs.put(JacksonJsonDeserializer.TYPE_MAPPINGS, typeMappings);
-
-        return new DefaultKafkaConsumerFactory<>(configs);
+    public DefaultErrorHandler errorHandler(KafkaTemplate<Long, Object> kafkaTemplate) {
+        var recoverer = new DeadLetterPublishingRecoverer(kafkaTemplate);
+        return new DefaultErrorHandler(recoverer, new FixedBackOff(5000L, 3L));
     }
 
     @Bean
-    public ProducerFactory<String, Object> producerFactory(
-            @Value("${spring.kafka.producer.bootstrap-servers}") String bootstrapServers
-    ) {
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
-
-        return new DefaultKafkaProducerFactory<>(configs);
-    }
-
-    @Bean
-    public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
-        return new KafkaTemplate<>(producerFactory);
-    }
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
-            ConsumerFactory<String, Object> consumerFactory,
-            KafkaTemplate<String, Object> kafkaTemplate) {
-        var factory = new ConcurrentKafkaListenerContainerFactory<String, Object>();
-        var errorHandler = new DefaultErrorHandler(new DeadLetterPublishingRecoverer(kafkaTemplate));
+    public ConcurrentKafkaListenerContainerFactory<Long, Object> kafkaListenerContainerFactory(
+            ConsumerFactory<Long, Object> consumerFactory,
+            DefaultErrorHandler errorHandler) {
+        var factory = new ConcurrentKafkaListenerContainerFactory<Long, Object>();
         factory.setConsumerFactory(consumerFactory);
         factory.setCommonErrorHandler(errorHandler);
 
@@ -72,7 +33,18 @@ public class KafkaConsumerConfig {
     }
 
     @Bean
-    public NewTopic createTopic(@Value("${kafka.topic-name}") String topic) {
+    public NewTopic emailTopic(
+            @Value("${app.kafka.topic-name}") String topic,
+            @Value("${app.kafka.partitions}") int partitions,
+            @Value("${app.kafka.replicas}") int replicas) {
+        return TopicBuilder.name(topic)
+                .partitions(partitions)
+                .replicas(replicas)
+                .build();
+    }
+
+    @Bean
+    public NewTopic emailDltTopic(@Value("${app.kafka.dlt-topic-name}") String topic) {
         return TopicBuilder.name(topic)
                 .partitions(1)
                 .replicas(1)
--- task-tracker-email-sender/src/main/java/com/walking/emailSender/service/impl/KafkaConsumerServiceImpl.java
+++ task-tracker-email-sender/src/main/java/com/walking/emailSender/service/impl/KafkaConsumerServiceImpl.java
@@ -4,17 +4,27 @@ import com.walking.emailSender.dto.MessageDto;
 import com.walking.emailSender.service.KafkaConsumerService;
 import com.walking.emailSender.service.MailSenderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class KafkaConsumerServiceImpl implements KafkaConsumerService {
     private final MailSenderService mailSenderService;
 
     @Override
-    @KafkaListener(topics = "${kafka.topic-name}")
+    @KafkaListener(topics = "${app.kafka.topic-name}")
     public void consumeMessage(MessageDto messageDto) {
-        mailSenderService.sendMessage(messageDto);
+        log.debug("Received email task for: {}", messageDto.getEmail());
+
+        try {
+            mailSenderService.sendMessage(messageDto);
+            log.debug("Email successfully sent to: {}", messageDto.getEmail());
+        } catch (Exception e) {
+            log.error("Error processing email task for: {}", messageDto.getEmail(), e);
+            throw e;
+        }
     }
 }
