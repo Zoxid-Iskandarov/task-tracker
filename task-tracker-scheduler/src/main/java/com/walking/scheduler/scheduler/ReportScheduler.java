b826480bf7427a58a6218a5a1a47b413c622b240
Zoxid-Iskandarov <iskandarovzoxid27@gmail.com>
2026-05-18 01:15:25 +0500

    feat(scheduler): add ReportScheduler with configurable cron and batch size

--- task-tracker-scheduler/src/main/java/com/walking/scheduler/scheduler/ReportScheduler.java
+++ task-tracker-scheduler/src/main/java/com/walking/scheduler/scheduler/ReportScheduler.java
@@ -1,17 +1,25 @@
 package com.walking.scheduler.scheduler;
 
-import com.walking.scheduler.service.ReportGeneratorService;
+import com.walking.scheduler.service.ReportGenerationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class ReportScheduler {
-    private final ReportGeneratorService reportGeneratorService;
+    private final ReportGenerationService reportGenerationService;
 
-    @Scheduled(cron = "${scheduler.cron}")
-    public void scheduleDailyReport() {
-        reportGeneratorService.generateDailyReport();
+    @Value("${app.scheduler.batch-size}")
+    private final int batchSize;
+
+    @Scheduled(cron = "${app.scheduler.cron}")
+    public void sendDailyReport() {
+        boolean hasMore = true;
+
+        while (hasMore) {
+            hasMore = reportGenerationService.processBatch(batchSize);
+        }
     }
 }
