3804c0b861e359ce1ba895d734f190cd0c8bc8d5
Zoxid-Iskandarov <iskandarovzoxid27@gmail.com>
2026-07-31 18:03:46 +0500

    test(auth): update IntegrationTestBase with minio container

--- task-tracker-backend/src/test/java/com/walking/backend/integration/IntegrationTestBase.java
+++ task-tracker-backend/src/test/java/com/walking/backend/integration/IntegrationTestBase.java
@@ -1,6 +1,7 @@
 package com.walking.backend.integration;
 
 import com.redis.testcontainers.RedisContainer;
+import com.walking.backend.security.authentication.TokenService;
 import com.walking.backend.service.KafkaProducerService;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
@@ -11,10 +12,12 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.MinIOContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.lifecycle.Startables;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -25,6 +28,10 @@ public abstract class IntegrationTestBase {
     private static final String POSTGRES_IMAGE_NAME = "postgres:16-alpine";
     private static final String REDIS_IMAGE_NAME = "redis:8-alpine";
     private static final String KAFKA_IMAGE_NAME = "apache/kafka:4.1.1";
+
+    private static final DockerImageName MINIO_IMAGE = DockerImageName
+            .parse("quay.io/minio/minio:RELEASE.2025-09-07T16-13-09Z")
+            .asCompatibleSubstituteFor("minio/minio");
 
     @Container
     @ServiceConnection
@@ -38,15 +45,24 @@ public abstract class IntegrationTestBase {
     @ServiceConnection
     protected static final KafkaContainer kafka = new KafkaContainer(KAFKA_IMAGE_NAME);
 
+    @Container
+    private static final MinIOContainer minio = new MinIOContainer(MINIO_IMAGE);
+
     static {
-        Startables.deepStart(postgres, redis, kafka).join();
+        Startables.deepStart(postgres, redis, kafka, minio).join();
     }
 
     @DynamicPropertySource
     static void overrideProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.kafka.producer.bootstrap-servers", kafka::getBootstrapServers);
+        registry.add("app.minio.endpoint", minio::getS3URL);
+        registry.add("app.minio.access-key", minio::getUserName);
+        registry.add("app.minio.secret-key", minio::getPassword);
     }
 
     @MockitoSpyBean
     private KafkaProducerService kafkaProducerService;
+
+    @MockitoSpyBean
+    private TokenService tokenService;
 }
--- task-tracker-backend/src/test/resources/sql/data.sql
+++ task-tracker-backend/src/test/resources/sql/data.sql
@@ -1,42 +1,33 @@
+-- Пользователь 1: john_doe
+-- Пароль: password123
 INSERT INTO users (id, username, email, password)
-VALUES (1, 'Zoxka', 'san781617@gmail.com',
-        '$2a$10$YYaAaFP54K74pbaXJj36d.KlNcBKXTwqT71qEndV.hSGc5TVDTsdS'), --Zox617
-       (2, 'Ivan', 'ivan99@gmail.com',
-        '$2a$10$EaZaBaEkswJl939P1D.iJ.uY7yOA0X.XP5/ojImDx.h9..xCudCDW'); --Ivan99
+VALUES (1,
+        'john_doe',
+        'john.doe@example.com',
+        '$2a$12$Y0nwjC.OjSWTUxNVnGvbfudznHKI.gctraIcMJMmDT9SRcirrv00m');
 
-SELECT setval('users_id_seq', 2);
+INSERT INTO user_profile (user_id, display_name, avatar_url, bio, created, updated)
+VALUES (1,
+        'John Doe',
+        'https://example.com/avatars/john.jpg',
+        'Software engineer from California. Love hiking and coding.',
+        NOW(),
+        NOW());
 
-INSERT INTO task (id, title, description, is_completed, created, updated, user_id)
-VALUES (1, 'Implement user registration',
-        'Create /auth/sign-up endpoint with validation and database persistence',
-        TRUE, now(), now(), 1),
-       (2, 'Configure Redis session storage',
-        'Integrate Spring Session with Redis and verify session lifecycle',
-        TRUE, now(), now(), 1),
-       (3, 'Add entity auditing',
-        'Enable created and updated timestamps using Spring Data auditing',
-        TRUE, now() - INTERVAL '2 days', now() - INTERVAL '2 days', 1),
-       (4, 'Implement password recovery',
-        'Send reset password email with token and implement reset form',
-        FALSE, now() - INTERVAL '2 days', now() - INTERVAL '2 days', 1),
-       (5, 'Optimize image upload',
-        'Add avatar preview and file size validation on upload',
-        FALSE, now(), now(), 1),
-       (6, 'Create login page UI',
-        'Build login form using HTML and Bootstrap 5 with error handling',
-        TRUE, now(), now(), 2),
-       (7, 'Integrate Kafka for email notifications',
-        'Publish email events to Kafka and consume them in email sender service',
-        TRUE, now(), now(), 2),
-       (8, 'Implement tasks REST API',
-        'Create CRUD endpoints for tasks with DTOs and validation',
-        TRUE, now() - INTERVAL '2 days', now() - INTERVAL '2 days', 2),
-       (9, 'Add task filtering',
-        'Filter tasks by status and creation date',
-        FALSE,now() - INTERVAL '2 days',now() - INTERVAL '2 days',2),
-       (10, 'Write service layer tests',
-        'Add unit and integration tests for task services',
-        FALSE, now(), now(), 2);
+-- Пользователь 2: jane_smith
+-- Пароль: strongPass456
+INSERT INTO users (id, username, email, password)
+VALUES (2,
+        'jane_smith',
+        'jane.smith@example.com',
+        '$2a$12$tPvdfT.Pv/GoZ7//eiZZmONK//NEtMJdE3DgfjTbTf065X3cYrbY2');
 
-SELECT setval('task_id_seq', 10);
+INSERT INTO user_profile (user_id, display_name, avatar_url, bio, created, updated)
+VALUES (2,
+        'Jane Smith',
+        'https://example.com/avatars/jane.jpg',
+        'Product manager. Coffee addict ☕',
+        NOW(),
+        NOW());
 
+SELECT setval('users_id_seq', 2)
